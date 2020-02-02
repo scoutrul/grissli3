c1941c1c4d8ff5ec2c50384e4a3a140a65caab4f
Anton <scoutrul@mail.ru>
2020-02-02 20:26:42 +0300

    filters with conditions

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,25 +1,72 @@
 <template>
   <div>
-    Search
+    <div>
+      <label>
+        Поиск
+        <input placeholder="Найти имена" v-model="searchInputValue" />
+      </label>
+      <hr />
+      <label>
+        <input type="checkbox" v-model="isToUpperCase" />
+        isToUpperCase
+      </label>
+      <label>
+        <input type="checkbox" v-model="isToLowerCase" />
+        isToLowerCase
+      </label>
+      <label>
+        <input type="checkbox" v-model="isToTranslit" />
+        isToTranslit
+      </label>
+      <label>
+        <input type="checkbox" v-model="isToReverseText" />
+        isToReverseText
+      </label>
+    </div>
+    <div>
+      {{
+        searchInputValue
+          | toUpperCase(isToUpperCase)
+          | toLowerCase(isToLowerCase)
+          | toTranslit(isToTranslit, rusToLat)
+          | toReverseText(isToReverseText)
+      }}
+    </div>
   </div>
 </template>
 
 <script>
-  
-import rusToLat from '@/mixins/rusToLat.js'
-import api from '@/mixins/api.js'
+import rusToLat from "@/mixins/rusToLat.js";
+import api from "@/mixins/api.js";
 export default {
-
   mixins: [api, rusToLat],
+  data: () => ({
+    searchInputValue: "",
+    isToUpperCase: false,
+    isToLowerCase: false,
+    isToTranslit: false,
+    isToReverseText: false
+  }),
   created() {
-    console.log(this.rusToLat('Антон Головачев'))
-    console.log(this.getDataNamesFromAPI('Антон'));
-    console.log(this.getDataSureNamesFromAPI('Головачев'))
+    console.log(this.rusToLat("Антон Головачев"));
+    // console.log(this.getDataNamesFromAPI('Антон'));
+    // console.log(this.getDataSureNamesFromAPI('Головачев'))
+  },
+  filters: {
+    toUpperCase: (value, bool) => (bool ? value.toUpperCase() : value),
+    toLowerCase: (value, bool) => (bool ? value.toLowerCase() : value),
+    toTranslit: (value, bool, rusToLat) => (bool ? rusToLat(value) : value),
+    toReverseText: (value, bool) =>
+      bool
+        ? value
+            .split("")
+            .reverse()
+            .join("")
+        : value
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
--- src/mixins/api.js
+++ src/mixins/api.js
@@ -1,40 +1,35 @@
-import russian_names from '@/data/russian_names.json'
-import russian_surnames from '@/data/russian_surnames.json'
-
 export default {
-    data: () => ({
-        loading: true
-    }),
-    methods: {
-        getDataNamesFromAPI(query) {
-            return new Promise((resolve, reject) => {
-                const items = this.dataNamesFromDB().find((e) => e.Name === query);
-                setTimeout(() => {
-                    this.loading = false
-                    resolve({
-                        items
-                    })
-                }, 1000)
-            })
-        },
-        getDataSureNamesFromAPI(query) {
-            return new Promise((resolve, reject) => {
-                const items = this.dataSurnamesFromDB().find(
-                    (e) => e.Surname === query
-                )
-                setTimeout(() => {
-                    this.loading = false
-                    resolve({
-                        items
-                    })
-                }, 1000)
-            })
-        },
-        dataNamesFromDB() {
-            return russian_names
-        },
-        dataSurnamesFromDB() {
-            return russian_surnames
-        }
-    }   
-}
+  data: () => ({
+    loading: true
+  }),
+  methods: {
+    getDataNamesFromAPI(query) {
+      return new Promise((resolve, reject) => {
+        const items = this.dataNamesFromDB().find(e => e.Name === query);
+        setTimeout(() => {
+          this.loading = false;
+          resolve({
+            items
+          });
+        }, 1000);
+      });
+    },
+    getDataSureNamesFromAPI(query) {
+      return new Promise((resolve, reject) => {
+        const items = this.dataSurnamesFromDB().find(e => e.Surname === query);
+        setTimeout(() => {
+          this.loading = false;
+          resolve({
+            items
+          });
+        }, 1000);
+      });
+    },
+    dataNamesFromDB() {
+      return () => import(`@/data/russian_names.json`);
+    },
+    dataSurnamesFromDB() {
+      return () => import(`@/data/russian_surnames.json`);
+    }
+  }
+};
